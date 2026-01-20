88944df4387805413f774ee1ed319d6610129590
Panda9733 <marts.olbiks@edu.kekava.lv>
2026-01-20 10:02:31 +0200

    saites
    visas pārējās saites

---
+++ mājaslapa.js
@@ -0,0 +1,15 @@
+function poga(){
+    window.location.href="Kitesurfing.html"
+}
+function poga1(){
+    window.location.href="Boatsailing.html"
+}
+function poga2(){
+    window.location.href="Wakeboarding.html"
+}
+function poga3(){
+    window.location.href="Windsurfing.html"
+}
+function poga4(){
+    window.location.href="Mājaslapa.html"
+}
